9490c6e882548f40a8dd4c52ab9c500a4c46ff9a
Maxime Bosquet <maxbo11052@gmail.com>
2016-04-25 20:28:18 +0200

    3 to 4 mm & ajout du cuivre pour les flèches Line Ver

--- Tests de flexion/LineSim.m
+++ Tests de flexion/LineSim.m
@@ -12,20 +12,24 @@ mgToN = 0.00000981;
 
 %Polyester
 E_PET   = 4*10^9;   %Pa
-L_PET   = 0.03;     %m
+L_PET   = 0.04;     %m
 W_PET   = 0.01;     %m
 H_PET   = 0.00005;  %m
 
 %Cuivre
 E_Cu    = 110*10^9;   %Pa
-L_Cu    = 0.03;       %m
+L_Cu    = 0.04;       %m
 W_Cu    = [0 1 2 3 4 5 6 7 9 3.6];%mm
 W_Cu    = W_Cu./1000; %m
 %3.6 est pour le ground hachuré
-H_Cu    = 0.000035;   %m
+H_Cu    = ones(1,10)*0.000035;   %m
+H_Cu(1) = 0;
 
+%dimensions
+W = W_Cu + W_PET;
+H = H_Cu + H_PET;
 % Inertie
-I_PET = (W_PET*H_PET^3)/12;
+I = (W.*power(H,3))./12;
 
 % Force appliquée
 mg = 100;       %mg
@@ -34,7 +38,7 @@ F = mg*mgToN;   %N
 %% Calcul 
 % Traction.Line(i).YoungM
 for i = 1:10
-    Line.Ver(i).Fleche = (1/48)*((F*L_PET^3)/(Traction.Line(i).YoungSim*I_PET));
+    Line.Ver(i).Fleche = (1/48)*((F*power(L_PET,3))/(Traction.Line(i).YoungM*I(i)));
 end
 
 %% Comparaison des pentes en µm/mg
@@ -42,6 +46,7 @@ figure('name','Comparaison de la flexibilité des échantillons','NumberTitle','of
 
 display = [Line.Ver.Pente ; Line.Ver.Fleche];
 display(2,:) = display(2,:)*10^4;
+%*10^4 car 0.0015 [m/100mg] = 15[mµ/mg]
 bar(display','hist')
 
 xlim([0 11]);
